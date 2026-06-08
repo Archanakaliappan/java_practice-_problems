aee2ede8304e80af6d65c4e90646fa8db8974b4e
Archana <archanakkaliappan@gmail.com>
2026-06-08 16:22:59 +0530

    feat: Add Candy

--- Array/Candy/java/Solution.java
+++ Array/Candy/java/Solution.java
@@ -13,9 +13,9 @@ class Solution {
                 arr1[i]=1;
             }
         }
-       for(int i=n-2;i<=0;i--){
-            if(ratings[i]<ratings[i+1]){
-              arr2[i]=arr1[i-1]+1;
+       for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+              arr2[i]=arr2[i+1]+1;
             }
             else{
                 arr2[i]=1;
