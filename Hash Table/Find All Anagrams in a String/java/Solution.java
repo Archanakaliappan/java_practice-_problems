a76b00cba9f6d8e48a4666dde75638a34d6623f7
Archana <archanakkaliappan@gmail.com>
2026-06-12 17:03:50 +0530

    feat: Add Find All Anagrams in a String

---
+++ Hash Table/Find All Anagrams in a String/java/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int l = 0;
+        int s1 = s.length();
+        int s2 = p.length();
+        int arr1[] = new int[26];
+        int arr2[] = new int[26];
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < s2; i++) {
+            arr1[s.charAt(i) - 'a']++;
+            arr2[p.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(arr1, arr2)) {
+            list.add(l);
+        }
+        for (int i = s2; i < s1; i++) {
+            //expand
+            arr1[s.charAt(i) - 'a']++;
+            arr1[s.charAt(l) - 'a']--;//shrink
+            l++;
+            if (Arrays.equals(arr1, arr2)) {
+                list.add(l);
+            }
+
+        }
+        return list;
+    }
+}
