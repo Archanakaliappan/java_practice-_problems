7c2310b1d06ed85ee56552f26be816c7ddfaa53f
Archana <archanakkaliappan@gmail.com>
2026-06-15 16:53:38 +0530

    feat: Add Sort an Array

--- Array/Sort an Array/java/Solution.java
+++ Array/Sort an Array/java/Solution.java
@@ -1,33 +1,16 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length - 1);
-        return nums;
-    }
-
-    private void mergeSort(int[] nums, int left, int right) {
-        if (left >= right) return;
-
-        int mid = left + (right - left) / 2;
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
-        merge(nums, left, mid, right);
-    }
-
-    private void merge(int[] nums, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
-        int i = left, j = mid + 1, k = 0;
-
-        while (i <= mid && j <= right) {
-            if (nums[i] <= nums[j]) {
-                temp[k++] = nums[i++];
-            } else {
-                temp[k++] = nums[j++];
+    //    for(int i=0;i<nums.length-1;i++){
+    //         for(int j=0;j<nums.length-1-i;j++){
+    //             if(nums[j]>nums[j+1]){
+    //                 int temp=nums[j];
+    //                 nums[j]=nums[j+1];
+    //                 nums[j+1]=temp;
+    //             }
+                Arrays.sort(nums);
+                return nums; 
             }
-        }
-
-        while (i <= mid) temp[k++] = nums[i++];
-        while (j <= right) temp[k++] = nums[j++];
-
-        System.arraycopy(temp, 0, nums, left, temp.length);
-    }
+        
 }
+
+    
