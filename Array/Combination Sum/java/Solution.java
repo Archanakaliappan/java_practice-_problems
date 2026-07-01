5051a2f5baf2aa2b653367712cf0f5711fecd62e
Archana <archanakkaliappan@gmail.com>
2026-07-01 16:13:13 +0530

    feat: Add Combination Sum

--- Array/Combination Sum/java/Solution.java
+++ Array/Combination Sum/java/Solution.java
@@ -1,25 +1,23 @@
 class Solution {
-    List<List<Integer>> result = new ArrayList<>();
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        backtrack(new ArrayList<>(), candidates, target, 0);
-        return result;
+        List<List<Integer>> ans=new ArrayList<>();
+        ArrayList<Integer> list=new ArrayList<>();
+         comb(candidates,target,0,ans,list);
+         return ans;
     }
-
-    private void backtrack(List<Integer> currentPath, int[] nums, int target, int index) {
-        if (target == 0) {
-            result.add(new ArrayList<>(currentPath));
-            return;
-        }
-
-        if (target < 0) {
-            return;
-        }
-
-        for (int i = index; i < nums.length; i++) {
-            currentPath.add(nums[i]);
-            backtrack(currentPath, nums, target - nums[i], i);
-            currentPath.remove(currentPath.size() - 1);
-        }
+    public void comb(int arr[],int t,int indx,List<List<Integer>> ans,List<Integer> list){
+  if(t==0){
+    ans.add(new ArrayList<>(list));
+    return;
+  }
+  if(t<0 || indx==arr.length){
+    return;
+  }
+  //skip the next indx
+    list.add(arr[indx]);
+    comb(arr,t-arr[indx],indx,ans,list);
+    list.remove(list.size()-1);
+    comb(arr,t,indx+1,ans,list);
+   
     }
 }
