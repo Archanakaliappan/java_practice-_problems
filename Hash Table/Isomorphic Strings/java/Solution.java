6c6fab2462a1984d1ad89b4bf47ee6f2c201b6e0
Archana <archanakkaliappan@gmail.com>
2026-07-03 11:46:21 +0530

    feat: Add Isomorphic Strings

--- Hash Table/Isomorphic Strings/java/Solution.java
+++ Hash Table/Isomorphic Strings/java/Solution.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashSet<Character> sset=new HashSet<>();
-        HashSet<Character> tset=new HashSet<>();
-        for(char i:s.toCharArray()){
-            sset.add(i);
-        }
-         for(char i:t.toCharArray()){
-            tset.add(i);
-        }
-        if(tset.size()!=sset.size()){
-            return false;
+        HashMap<Character,Integer> smap=new HashMap<>();
+        HashMap<Character,Integer> tmap=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(!smap.containsKey(s.charAt(i))){
+                smap.put(s.charAt(i),i);
+            }
+            if(!tmap.containsKey(t.charAt(i))){
+                tmap.put(t.charAt(i),i);
+            }
+            if(tmap.get(t.charAt(i))!=smap.get(s.charAt(i))){
+                return false;
+            }
         }
         return true;
     }
