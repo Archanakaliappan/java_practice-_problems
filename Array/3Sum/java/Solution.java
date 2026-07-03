22af961114841f24a1f168fa8231fca54a4f19c6
Archana <archanakkaliappan@gmail.com>
2026-07-03 10:27:22 +0530

    feat: Add 3Sum

--- Array/3Sum/java/Solution.java
+++ Array/3Sum/java/Solution.java
@@ -1,32 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int sum=0;
-      
-        List<List<Integer>> list=new ArrayList<>();
-       
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(i>0 && nums[i]==nums[i-1]){
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-              int k=i+1;
-               int e=nums.length-1;
-            while(k<e){
-                  sum=nums[i]+nums[k]+nums[e];
-                  if(sum>0){
+            
+            int k = i + 1;
+            int e = nums.length - 1; // Reset 'e' for every new 'i'
+            
+            while (k < e) {
+                int sum = nums[i] + nums[k] + nums[e]; // Calculate fresh sum
+                
+                if (sum > 0) {
                     e--;
-                  }
-                  else if(sum<0){
-                   k++;
-                  }
-                  else if(sum==0){
-                  list.add(Arrays.asList(nums[i],nums[k],nums[e]));
-                  k++;
-                  }
-                  while (nums[k] == nums[k-1] && k< e) {
+                } else if (sum < 0) {
+                    k++;
+                } else {
+                    // Found a valid triplet
+                    list.add(Arrays.asList(nums[i], nums[k], nums[e]));
+                    
+                    // Skip duplicates for the second element
+                    while (k < e && nums[k] == nums[k + 1]) {
                         k++;
+                    }
+                    // Skip duplicates for the third element
+                    while (k < e && nums[e] == nums[e - 1]) {
                         e--;
                     }
+                    
+                    
+                    k++;
+                    e--;
+                }
             }
         }
         return list;
